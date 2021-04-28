4a02c79d4b73d1ad1b975639e1a6d4eb2779f06e
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-04-27 21:24:43 -0300

    Add volume mapping and .gitignore

--- start-keyclock.sh
+++ start-keyclock.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:12.0.4
+docker run --volume "$(pwd)/keycloak-data":/opt/jboss/keycloak/standalone/data/ -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:12.0.4
